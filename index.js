64550448e6af5d8181c71f7e0e6ce08de70604ef
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-12-25 17:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  
+}
+  
