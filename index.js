d7a4d5695cc6db93ca80c929d98d2d3a9515991e
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2018-12-25 17:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  
+   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
 }
 
 function deepestChild() {
